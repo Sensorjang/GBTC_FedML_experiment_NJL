9cab8924ffaf71b8548092d927efbd8c9e691c4b
Extraordinary <3023538364@qq.com>
2024-03-07 09:54:51 +0800

    加入fashionmnist

--- python/fedml/model/cv/cnn.py
+++ python/fedml/model/cv/cnn.py
@@ -187,5 +187,21 @@ class CNN_WEB(nn.Module):
 
 
 
+class FashionMNIST_CNN(nn.Module):
+    def __init__(self):
+        super(FashionMNIST_CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 
